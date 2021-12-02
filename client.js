6efa22b60ee6931161c40b9d9d4b459a35c188ba
A E <aelmollaz@gmail.com>
2021-12-02 12:35:12 +0000

    Sends name on connection

--- client.js
+++ client.js
@@ -1,13 +1,19 @@
 const net = require("net");
 const {IP, PORT} = require('./constants') ;
 // establishes a connection with the game server
-const connect = function () {
+const connect = function () {   // main function that handles the connection
   const conn = net.createConnection({
     host: IP,           // IP address here,
     port: PORT                // PORT number here,
   });
 
+  conn.on('connect', () => {  //prints connection successful when connected
+    console.log('Successfully connected to the game server');
+    conn.write("Name: AE");
+  });
+ 
   conn.setEncoding("utf8"); // interpret incoming data as text
+
   conn.on('data', (data) => {      // recieves text sent from server
     console.log('Server says: ', data);
   });
