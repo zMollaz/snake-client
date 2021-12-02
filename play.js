a83e36ba26e921c5c1c46b276c52b7348e6d98d4
A E <aelmollaz@gmail.com>
2021-12-02 13:39:45 +0000

    Registered exit key to the stdin

--- play.js
+++ play.js
@@ -1,4 +1,20 @@
 const {net, connect} = require('./client')
 
+const handleUserInput = function (key) {
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
+const setupInput = function () {  //function that handles user inputs
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
 console.log("Connecting ...");
 connect();
+setupInput();
