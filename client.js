e678cd436857b7677e2ab466d6e3705c53a7a2a9
A E <aelmollaz@gmail.com>
2021-12-02 12:54:34 +0000

    Checked the snake movements

--- client.js
+++ client.js
@@ -7,11 +7,15 @@ const connect = function () {   // main function that handles the connection
     port: PORT                // PORT number here,
   });
 
-  conn.on('connect', () => {  //prints connection successful when connected
-    console.log('Successfully connected to the game server');
-    conn.write("Name: AE");
+  conn.on('connect', () => {  // establishes a connection
+    console.log('Successfully connected to the game server'); //prints connection successful
+    conn.write("Name: AE"); //pritns initials upon connection
+    //conn.write("Move: up"); // registers a move up movement on connection
+    //conn.write("Move: right");
+    //conn.write("Move: down");
+    //conn.write("Move: left");
   });
- 
+
   conn.setEncoding("utf8"); // interpret incoming data as text
 
   conn.on('data', (data) => {      // recieves text sent from server
