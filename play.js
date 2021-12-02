677843d968b7079759b7f86ea368defc71174a12
A E <aelmollaz@gmail.com>
2021-12-02 13:47:52 +0000

    Added input.js

--- play.js
+++ play.js
@@ -1,19 +1,10 @@
-const {net, connect} = require('./client')
+const {net, connect} = require('./client');
+const {setupInput} = require('./input');
 
-const handleUserInput = function (key) {
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
 
-const setupInput = function () {  //function that handles user inputs
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
+
+
+
 
 console.log("Connecting ...");
 connect();
